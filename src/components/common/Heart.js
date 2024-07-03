5690f32fe676af2188df8adff2c1770055b86923
Vu Hieu <vuminhhieu@gmail.com>
2024-07-03 14:37:27 +0700

    Rename file, folder

--- src/components/common/Heart.js
+++ src/components/common/Heart.js
@@ -1,13 +1,13 @@
 import React, { useState } from 'react'
 import { FaRegHeart } from "react-icons/fa";
 import { FaHeart } from "react-icons/fa";
-const Heart = ({ sizeDiv, sizeIcon }) => {
+const Heart = ({ customStyle, sizeIcon }) => {
   const [isHeart, setIsHeart] = useState(true)
   const handClick = () => {
     setIsHeart(!isHeart)
   }
   return (
-    <div className={`cursor-pointer mx-[10px] bg-gradient-to-r to-gray from-white ${sizeDiv} flex items-center justify-center border border-white-2 rounded-xl`} onClick={handClick}>
+    <div className={`cursor-pointer mx-[10px] bg-gradient-to-r to-gray from-white ${customStyle} flex items-center justify-center border border-white-2 rounded-xl`} onClick={handClick}>
       {isHeart ? <FaRegHeart color='#6100C2' size={sizeIcon} /> : <FaHeart color='#6100C2' size={sizeIcon} />}
     </div>
   )
