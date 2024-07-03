bb7632742008b18e9df097ad3d6721c935d389ed
Vu Hieu <vuminhhieu@gmail.com>
2024-07-03 15:08:12 +0700

    fix sidebar style

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,43 +5,43 @@ import { Link } from 'react-router-dom'
 
 const Sidebar = () => {
   return (
-    <div className='bg-black w-[274px] h-[1024px]'>
+    <div className='bg-black w-full max-w-[274px] pl-[40px]'>
       <Link to={path.PUBLIC}>
-        <h3 className='flex pt-[35px] ml-[40px] '>
+        <h3 className='flex pt-[35px] '>
           <Logo />
           <span className='font-poppins size-[22px] text-white ml-3 mt-2' >WATCH</span>
         </h3>
       </Link>
-      <div className=''>
-        <Link to={path.HOME}><div className='flex mt-[45px] ml-[40px] '>
+      <div className='flex flex-col gap-[35px] mt-[35px]'>
+        <Link to={path.HOME}><div className='flex'>
           <Icon1 />
-          <span className='font-poppins size-[22px] text-white ml-3 ' >Home</span>
+          <span className='font-poppins size-[22px] text-white ml-3' >Home</span>
         </div></Link>
-        <Link to={path.Favourites}><div className='flex mt-[35px] ml-[40px] '>
+        <Link to={path.Favourites}><div className='flex'>
           <Icon2 />
-          <span className='font-poppins size-[22px] text-white ml-3 ' >Favourites</span>
+          <span className='font-poppins size-[22px] text-white ml-3' >Favourites</span>
         </div></Link>
-        <Link to={path.Trending}><div className='flex mt-[35px] ml-[40px] '>
+        <Link to={path.Trending}><div className='flex'>
           <Icon3 />
           <span className='font-poppins size-[22px] text-white ml-3 ' >Trending</span>
         </div></Link>
-        <Link to={path.HOME}><div className='flex mt-[35px] ml-[40px] '>
+        <Link to={path.HOME}><div className='flex'>
           <Icon4 />
-          <span className='font-poppins size-[22px] text-white ml-3 ' >Coming</span>
+          <span className='font-poppins size-[22px] text-white ml-3'>Coming</span>
         </div></Link>
-        <Link to={path.HOME}><div className='flex mt-[125px] ml-[40px] '>
+        <Link to={path.HOME}><div className='flex mt-[125px]'>
           <Icon5 />
-          <span className='font-poppins size-[22px] text-white ml-3 ' >Community</span>
+          <span className='font-poppins size-[22px] text-white ml-3'>Community</span>
         </div></Link>
-        <Link to={path.HOME}><div className='flex mt-[35px] ml-[40px] '>
+        <Link to={path.HOME}><div className='flex'>
           <Icon6 />
-          <span className='font-poppins size-[22px] text-white ml-3 ' >Social</span>
+          <span className='font-poppins size-[22px] text-white ml-3' >Social</span>
         </div></Link>
-        <Link to={path.HOME}><div className='flex mt-[265px] ml-[40px] '>
+        <Link to={path.HOME}><div className='flex mt-[265px]'>
           <Icon7 />
-          <span className='font-poppins size-[22px] text-white ml-3 ' >Settings</span>
+          <span className='font-poppins size-[22px] text-white ml-3'>Settings</span>
         </div></Link>
-        <Link to={path.HOME}><div className='flex mt-[35px] ml-[40px] '>
+        <Link to={path.HOME}><div className='flex'>
           <Icon8 />
           <span className='font-poppins size-[22px] text-white ml-3 ' >Logout</span>
         </div></Link>
