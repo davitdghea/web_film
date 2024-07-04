5f1867848c127952932578dbf05f194cb257f035
Vu Hieu <vuminhhieu@gmail.com>
2024-07-04 16:29:38 +0700

    fix FilmDetails

--- src/components/Trending.js
+++ src/components/Trending.js
@@ -1,11 +1,41 @@
-import React from 'react'
-import NavBar from '../components/Navbar'
+import React, { useContext } from 'react'
+import MovieCards from '../components/common/MovieCards'
+import FilmDetails from '../components/common/FilmDetails'
+import {AipContext} from '../stores/trending/index.js'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import Navbar from './Navbar.js'
+
+var settings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 4,
+  slidesToScroll: 4
+}; 
 
 const TrendingComponent = () => {
+  const { movies } = useContext(AipContext)
   return (
-    <div className='bg-zinc-500'>
-        <NavBar />
+    <div >
+      <div className='h-[129px]'>
+        <Navbar/>
+      </div>
+      <div className='text-white h-[30px] font-semibold text-left ml-[25px] text-[20px] '>Trending at this moment</div>
+      <div className='bg-gradient-to-r from-gray-500 to-gray-900 w-full max-w-[calc(100vw-274px)] mt-[16px] '>
+        
+        <Slider {...settings}>
+        {movies.map(el =>(
+          <MovieCards title={el.title} date={el.release_date} img={el.backdrop_path}  co='w-[274px] h-[301px]' />
+        )
+        )}
+       </Slider>
+      </div>
+      <FilmDetails/>
     </div>
+    
+    
   )
 }
 
--- src/components/common/FilmDetails.js
+++ src/components/common/FilmDetails.js
@@ -1,25 +1,29 @@
 import React from 'react'
 import Heart from './Heart'
 import Button from './Button'
+import StarRatings from 'react-star-ratings'
+
 
 const FilmDetails = () => {
     return (
-        <div className='w-full h-[550px] flex items-center justify-between gap-2'>
-            <div className='bg-amber-200 w-1/2 h-[431px] flex items-center justify-center'>
-                <span>img</span>
+        <div className='w-full h-[550px] flex items-center justify-between gap-[24px]'>
+            <div className=' w-1/2 h-[431px]  flex items-center justify-center ' >
+                <span>
+                    <img src="https://www.simpleimageresizer.com/_uploads/photos/8be27dd6/a23f1b217acfa7375285a925d4865c22_534x431.jpg" alt=''></img>
+                </span>
             </div>
             <div className='w-1/2 h-[300px] text-white'>
                 <div className=' flex justify-between'>
-                    <h2 className='text-2xl font-semibold'>House of Wealth</h2>
-                    <span>star</span>
+                    <h2 className='text-[32px] font-semibold'>House of Wealth</h2>
+                    <StarRatings  />
                 </div>
                 
-                <span className='flex justify-between w-[250px] mt-[12px]'>
+                <span className='flex justify-between w-[250px] mt-[12px] text-[18px]'>
                     <span>2023</span>
                     <span>Drama</span>
                     <span>2h 38m</span>
                 </span>
-                <p className='text-start mt-[20px]'>
+                <p className='text-start mt-[20px] text-[16px] mr-[30px]'>
                     The movie follows the lives of a wealthy family, the Johnsons, 
                     who appear to have it all: a grand mansion, luxurious cars, and expensive designer clothing. However, behind the facade of their lavish lifestyle, there are deep-seated tensions and secrets that threaten to tear the family apart.
                 </p>
